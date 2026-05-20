92b5373c86a5e4c4f074ecae04e7de466bcc077c
KrzakalaPaul <62666596+KrzakalaPaul@users.noreply.github.com>
2026-05-20 23:23:59 +0200

    update train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,10 +2,12 @@
 
 #SBATCH --output=logs/job%j.log
 #SBATCH --error=logs/job%j.err
-#SBATCH --time=02:00:00
+#SBATCH --time=06:00:00
 #SBATCH --partition=V100
 #SBATCH --gpus=1
 
 set -x
 
-srun python -u 3_train.py
+srun python -u 3_train.py --labelled_dataset_name massspecgym --candidate_map_name 256_candidatates_by_mass --k_candidates 128 --encoder_mol chemberta_13M --wandb_run_name dev_13M
+
+srun python -u 3_train.py --labelled_dataset_name massspecgym --candidate_map_name 256_candidatates_by_mass --k_candidates 128 --encoder_mol chemberta_100M --wandb_run_name dev_100M

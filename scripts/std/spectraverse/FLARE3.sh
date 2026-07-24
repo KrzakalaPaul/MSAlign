43005764cf453569d18949d3d25d10ea60f60c12
PaulKrzakala <paul.krzakala@gmail.com>
2026-07-24 08:06:39 +0200

    set proper candidates for FLARE

--- scripts/std/spectraverse/FLARE3.sh
+++ scripts/std/spectraverse/FLARE3.sh
@@ -9,5 +9,5 @@
 set -x
 ulimit -n 65536 # Fix for FLARE dataloader workers
 
-srun python -u 3_train.py --labelled_dataset_name spectraverse --candidate_map_name official_candidates_by_mass --model FLARE --config default --wandb_run_name FLARE_spectraverse_formula_seed3 --split_method formula_seed3
+srun python -u 3_train.py --labelled_dataset_name spectraverse --candidate_map_name 256_candidates_by_mass --model FLARE --config default --wandb_run_name FLARE_spectraverse_formula_seed3 --split_method formula_seed3
 
